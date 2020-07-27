8cdf200fac1da2893c1f3f01185c9ed8ae8f08b6
Antonio V. Leonti <53806445+antoniovleonti@users.noreply.github.com>
2020-07-26 23:46:33 -0400

    reformat

--- C/TrailingZeroBits.c
+++ C/TrailingZeroBits.c
@@ -14,7 +14,7 @@ int main(){
     int input = 0;
     int output = solve(input);
 
-    printf("There are %d trailing zero-bits in %d.", input, output);
+    printf("There are %d trailing zero-bits in %d.", output, input);
 
     getch(); return 0;
 }
@@ -22,10 +22,9 @@ int main(){
 int solve(int n){
     int ret = 0;
     // the number of trailing zeros for 0 is the number of bits in an int.
-    while(!(n % 2) && ret < 8 * sizeof(int) - 1){ // If the last bit is 0
+    while(!(n % 2) && ret < 8 * sizeof(int)){ // If the last bit is 0
         n >>= 1; // shift bits to the right
         ret++; // and increase the return value by 1.
     }
-
     return ret;
 }
